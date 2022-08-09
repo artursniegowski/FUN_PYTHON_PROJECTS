6752843b7936992f80272a91b2f24db8c6e349f2
artur.sniegowski <username@users.noreply.github.com>
2022-08-09 13:10:04 +0200

    Updated password manager GUI

--- Password_manager/main.py
+++ Password_manager/main.py
@@ -3,7 +3,7 @@ from random import randint
 import tkinter
 from tkinter import messagebox
 import pyperclip
-
+import json
 
 # constant - settings
 MAIN_WINDOW_TITLE = "Password Manager"
@@ -19,10 +19,51 @@ LABEL_EMAIL_USERNAME_TEXT = "Email/Username:"
 LABEL_PASSWORD_TEXT = "Password:"
 # buttons settings
 BUTTON_ADD_TEXT = "ADD"
-BUTTON_GENERATE_PASSWORD_TEXT = "Generate Password" 
+BUTTON_GENERATE_PASSWORD_TEXT = "Generate Password"
+BUTTON_SEARCH_TEXT = "Search"  
 # entry settings
 ENTRY_EMAIL_START_VALUE = 'EXAMPLE@gmail.com'
+# FILE name settings
+FILE_NAME_JSON = "data.json"
 
+# ==============================================================
+
+
+# ---------------------------- SEARCHING DATA ------------------------------- #
+def search_data():
+    """
+    search for the website name and its value in JSON file
+    """
+    website_name = inpput_field_website.get()
+
+    # checking if the website name exists
+    if website_name:
+
+        data = {}
+        try:
+            # reading the json file
+            with open(FILE_NAME_JSON,'r') as f:
+                data = json.load(f)
+        # cant find the file
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            messagebox.showerror(title=FILE_NAME_JSON, message=f"The {FILE_NAME_JSON} can't be found or is empty")
+        else:
+            if website_name in data: # checking if the website exists in the file
+                email = data[website_name]['email']
+                password = data[website_name]['password']
+                
+                # deleting whatever is in the inputs
+                inpput_password.delete(0,tkinter.END)
+                inpput_username.delete(0,tkinter.END)
+
+                # writing the data from the json file inot inputs
+                inpput_password.insert(0,password)
+                inpput_username.insert(0,email)
+            else:
+                messagebox.showwarning(title=website_name + " missing!", message=f"The {website_name} dosen't exists in {FILE_NAME_JSON}.")
+
+    else: 
+        messagebox.showinfo(title="Website",message="The Website field can't be empty")
 
 
 
@@ -52,7 +93,12 @@ def save_to_file():
     user_name = inpput_username.get()
     password_new = inpput_password.get()
 
-    data = website_data + " | " + user_name + " | " + password_new +"\n"
+    data = {
+        website_data : {
+            'email': user_name,
+            'password': password_new,
+        },
+    }
 
     # simple validation - checking if fields are not empty
     if website_data and user_name and password_new: 
@@ -62,12 +108,30 @@ def save_to_file():
                 \nPassword: {password_new} \n Is it ok to save?")
 
         if is_ok:
-            with open("data.txt",'a') as f:
-                f.write(data)
-
-            # deleting the values in the entries
-            inpput_field_website.delete(0,tkinter.END)
-            inpput_password.delete(0,tkinter.END)
+            old_data = {}
+            try:
+                # open existing data
+                with open(FILE_NAME_JSON,'r') as f:
+                    # reading old data
+                    old_data = json.load(f)
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                # if file dosent exists - create the file
+                # or if the file is empty 
+                # and save all the data 
+                with open(FILE_NAME_JSON,'w') as f:
+                    # saving updated data
+                    json.dump(data, f, indent=4)
+            else:
+                # updating old data
+                old_data.update(data)
+                # save all the data 
+                with open(FILE_NAME_JSON,'w') as f:
+                    # saving updated data
+                    json.dump(old_data, f, indent=4)
+            finally:
+                # deleting the values in the entries
+                inpput_field_website.delete(0,tkinter.END)
+                inpput_password.delete(0,tkinter.END)
 
     else:
         messagebox.showerror(title="Field is required!!",\
@@ -100,8 +164,8 @@ label_password.grid(row=3,column=0)
 
 
 # creating entries
-inpput_field_website = tkinter.Entry(width=51)
-inpput_field_website.grid(row=1,column=1,columnspan=2)
+inpput_field_website = tkinter.Entry(width=32)
+inpput_field_website.grid(row=1,column=1)
 inpput_field_website.focus() # put the cursor into the widget
 
 inpput_username = tkinter.Entry(width=51)
@@ -118,6 +182,10 @@ button_add.grid(row=4,column=1,columnspan=2)
 button_generate_password = tkinter.Button(text=BUTTON_GENERATE_PASSWORD_TEXT,\
     pady=2, command=generate_password)
 button_generate_password.grid(row=3,column=2)
+
+button_search = tkinter.Button(text=BUTTON_SEARCH_TEXT,width=14,pady=2,\
+    command=search_data)
+button_search.grid(row=1,column=2)
 
 # waiting on the user to close the window
 window_main.mainloop()
--- README.md
+++ README.md
@@ -61,7 +61,9 @@ This program is used to store and generate passwords. It requires filling few fi
 Website - this is used only as description for future identification. So it is easier to deduct what this password is for.
 Email/Username - which is prepopulated but can be change to any username / email address.
 Password - here we fill in the password we want to store, or we can choose to autogenerate a password with the Generate Password button, which results also in copying the newly generated password into our clipboard and be ready to use it right away. 
-After completing the form, we can add the password to our list of passwords, which will be saved in a data.txt file. Hitting the Add button will result in another pop up window asking for confirmation of adding a new password to the list. 
+After completing the form, we can add the password to our list of passwords, which will be saved in a data.JSON file. Hitting the Add button will result in another pop up window asking for confirmation of adding a new password to the list. 
+There is also the search button which will look for the given website name, and retrieve the data from the data.json file (if it exists)
+and populate the password and email/username fields. If an error occurs or the user will submit an invalid input, the program will indicate this with a pop-up message box.  
 
 # Pokemon_Search_API
 It is a program which uses the API https://pokeapi.co/ to look through the database of the pokemons. 
